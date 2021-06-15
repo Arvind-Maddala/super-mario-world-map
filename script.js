c5d015fe6065210aa63f1081a817b971d10467c6
unknown <arvindprince10@gmail.com>
2021-06-15 21:44:19 +0530

    new file added

---
+++ script.js
@@ -0,0 +1,97 @@
+
+// Create the script tag, set the appropriate attributes
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCOQcFm1xU46vmFRWIEk9RXd4OisecsmkU&map_ids=18b4a38d036abff2&callback=initMap';
+script.defer = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function() {
+  // JS API is loaded and available
+	map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 34.66793906192621, lng: 135.43047658225953},
+    zoom: 18,
+    mapId: '18b4a38d036abff2',
+    mapTypeControl: false,
+		fullscreenControl: false,
+		streetViewControl: false,
+  });
+  
+  const markers = [
+		[
+			"Yoshi's House",
+			34.66669734177897,
+			135.4309054875526,
+			'./images/yoshi_house.svg',
+			38,
+			31,
+		],
+		[
+			'You Are Here',
+			34.66767112713121,
+			135.4297887322531,
+			'./images/pointer.svg',
+			30,
+			47.8,
+		],
+		[
+			'Ghost House',
+			34.66832715150856,
+			135.43292762674864,
+			'./images/ghosthouse.svg',
+			40,
+			48,
+		],
+		['Castle', 34.66775608022106, 135.43139547897843, './images/castle.svg', 40, 53],
+		['Warp Pipe', 34.66739738878135, 135.43225049775214, './images/pipe.svg', 38, 42.5],
+		['Star World', 34.667959023359266, 135.42866400953733, './images/star.svg', 38, 38],
+		[
+			'Donut Plains',
+			34.66830355359945,
+			135.4320565322381,
+			'./images/hill_with_eyes.svg',
+			50,
+			60.7,
+		],
+		[
+			'Donut Plains',
+			34.66829411443392,
+			135.43231361996433,
+			'./images/hill_with_eyes.svg',
+			50,
+			60.7,
+		],
+		[
+			'Donut Plains',
+			34.6683781779677,
+			135.43217016043528,
+			'./images/hill_with_eyes.svg',
+			50,
+			60.7,
+		],
+	];
+
+  for (let i = 0; i < markers.length; i++) {
+		const currMarker = markers[i];
+
+  const marker = new google.maps.Marker({
+    position:  { lat: currMarker[1], lng: currMarker[2]}, 
+    map,
+    title: currMarker[0],
+    icon: { 
+      url: currMarker[3],
+      scaledSize: new google.maps.Size(currMarker[4],currMarker[5])
+    },
+    animation: google.maps.Animation.DROP
+  });
+  const infowindow = new google.maps.InfoWindow({
+    content: currMarker[0],
+  });
+  marker.addListener('click', () => {
+    infowindow.open(map, marker);
+  });
+}
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
+ 
